79f1d4e8e92f37c3f59abc30a7f2fbbac54e3803
MennaSayed46 <mennaabdelateef910@gmail.com>
2025-10-01 18:15:54 +0300

    Question01

--- Assignment04Basics/Program.cs
+++ Assignment04Basics/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace Assignment04Basics
+﻿namespace Demo
 {
     internal class Program
+
     {
+    
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+            //1 - Explain the difference between passing(Value type parameters) by value and by reference then write a suitable c# example.
+            #region Passing by value
+            //   A copy of the variable’s value is passed into the method.
+            //   Any changes inside the method do NOT affect the original variable.
+            #endregion
+            #region passing by reference
+            //The method receives the actual memory address of the variable. Any changes inside the method directly affect the original variable.
+            #endregion
+
+            ///passing by value example :
+            ///static void Swap(int x, int y)
+            ///{
+            ///    Console.WriteLine("swapping");
+            ///    int temp;
+            ///    temp = x;
+            ///    x = y;
+            ///    y = temp;
+            ///}
+            ///int A = 3;
+            ///int B = 5;
+            ///Console.WriteLine($"before the swapping :\n A={A} \n B={B}");
+            ///Swap(A, B);
+            ///    Console.WriteLine($"after the swapping :\n A={A} \n B={B}");
+            ///    
+
+            ///passing by reference example :
+            ///static void Swap(ref int x, ref int y)
+            ///{
+            ///    Console.WriteLine("swapping");
+            ///    int temp;
+            ///    temp = x;
+            ///    x = y;
+            ///    y = temp;
+            ///}
+            ///int A = 3;
+            ///int B = 5;
+            ///Console.WriteLine($"before the swapping :\n A={A} \n B={B}");
+            ///Swap(ref A, ref B);
+            ///Console.WriteLine($"after the swapping :\n A={A} \n B={B}");
+
+
+            #endregion
         }
     }
 }
--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,31 @@
 ﻿namespace Demo
 {
     internal class Program
-    {
+     
+{
+        static void Swap(int x, int y)
+        {
+            Console.WriteLine("swapping");
+            int temp;
+            temp = x;
+            x = y;
+            y = temp;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Question01
+                                                            //int x             passingByValue       
+            //1 - Explain the difference between passing(Value type parameters) by value and by reference then write a suitable c# example.
+               //if we passing parameter by value , we pass the value itself not the address ,
+               //if we pass paramter by rtefernce , we pass the address itself , then any changes will do int the function will affect on the real parameter .
+
+              
+            int A = 3;
+            int B = 5;
+            Console.WriteLine($"before the swapping :\n A={A} \n B={B}");
+            Swap(A,B)
+                Console.WriteLine($"after the swapping :\n A={A} \n B={B}");
+            #endregion
         }
     }
 }
