26b0193239c75dbd3b6fd5ec4adea27e78527975
MennaSayed46 <mennaabdelateef910@gmail.com>
2025-10-01 17:13:32 +0300

    Add project files.

---
+++ Assignment04Basics/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment04Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
